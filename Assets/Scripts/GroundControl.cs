13cd363d6c80bc84daa3cfbc76ffb4d6bb7acbc1
furkanlebit7 <furkanlebitt7@gmail.com>
2023-05-31 01:19:33 +0300

    project almost finished

---
+++ Assets/Scripts/GroundControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundControl : MonoBehaviour {
+
+	//Material texture offset rate
+	float speed = .5f;
+	public static float time=1f;
+	
+	//Offset the material texture at a constant rate
+	void Update () {
+		if(Time.deltaTime == 0){
+			time=1f;
+		}
+			time+=0.0001f;
+			float offset = Time.time * speed;
+			GetComponent<Renderer> ().material.mainTextureOffset = new Vector2 (0, -offset);
+	}
+
+}
